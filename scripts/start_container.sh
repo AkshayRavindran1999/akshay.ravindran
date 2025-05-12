32b69277328a20c8e0dff514fc05f381de5bd3e6
Akshay Ravindran <94100432+akshay-ravindran-cloud@users.noreply.github.com>
2025-05-12 19:33:38 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull akshayravindran99/simple-python-flask-app
+docker pull docker pull akshayravindran99/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 akshayravindran99/simple-python-flask-app
+docker run -d -p 5000:5000 docker pull akshayravindran99/simple-python-flask-app:latest
